909c8c81e6ff1aa83afeed7e20c62a0fe73fd47f
PoLoSaTaHata <as_yakimov@bk.ru>
2018-09-25 23:59:22 +0300

    Домашка

--- app/src/main/java/ru/polosatuk/mycard/CardActivity.java
+++ app/src/main/java/ru/polosatuk/mycard/CardActivity.java
@@ -2,20 +2,26 @@ package ru.polosatuk.mycard;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class CardActivity extends AppCompatActivity implements View.OnClickListener{
+public class CardActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView telegrammClick;
     private ImageView vkClick;
     private ImageView gitClick;
     private ImageView sendMessageClick;
     private EditText textMessage;
+    private LinearLayout mainL;
+    private LinearLayout.LayoutParams layoutParams;
 
     String linkToGit = "https://github.com/PoLoSkA";
     String linkToVK = "https://vk.com/smugas";
@@ -26,9 +32,20 @@ public class CardActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
         init();
+        disclaimer();
     }
 
-    private void init(){
+
+
+    private void disclaimer() {
+        TextView tvDisclaimer = new TextView(this);
+        tvDisclaimer.setText(getString(R.string.disclaimer));
+        tvDisclaimer.setLayoutParams(layoutParams);
+        mainL.addView(tvDisclaimer);
+    }
+
+    private void init() {
+        mainL = findViewById(R.id.mainL);
         textMessage = findViewById(R.id.textMessage);
         gitClick = findViewById(R.id.git_click);
         vkClick = findViewById(R.id.vk_click);
@@ -39,14 +56,19 @@ public class CardActivity extends AppCompatActivity implements View.OnClickListe
         telegrammClick.setOnClickListener(this);
         sendMessageClick.setOnClickListener(this);
 
+        layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        layoutParams.gravity = Gravity.END;
+
+
     }
+
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
+        switch (view.getId()) {
             case R.id.sendMessage:
-                 String message =textMessage.getText().toString();
-                if (message.trim().length() > 0){
+                String message = textMessage.getText().toString();
+                if (message.trim().length() > 0) {
                     sendSms(message);
                 }
                 break;
@@ -72,12 +94,12 @@ public class CardActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, R.string.no_app, Toast.LENGTH_LONG).show();
     }
 
-    private void sendSms(String message){
+    private void sendSms(String message) {
         String phoneNumber = "+79653811975";
         Intent smsIntent = new Intent(android.content.Intent.ACTION_VIEW);
         smsIntent.setType("vnd.android-dir/mms-sms");
-        smsIntent.putExtra("address",phoneNumber);
-        smsIntent.putExtra("sms_body",message);
+        smsIntent.putExtra("address", phoneNumber);
+        smsIntent.putExtra("sms_body", message);
         if (smsIntent.resolveActivity(getPackageManager()) != null)
             startActivity(smsIntent);
         else
